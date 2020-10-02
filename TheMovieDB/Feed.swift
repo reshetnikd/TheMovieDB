e479b5d90e01c7bb800ddee2f17e3ec84177207c
reshetnikd <dmitry.reshetnik@icloud.com>
2020-10-02 17:17:25 +0300

    Add data model according to API response

---
+++ TheMovieDB/Feed.swift
@@ -0,0 +1,32 @@
+//
+//  Feed.swift
+//  TheMovieDB
+//
+//  Created by Dmitry Reshetnik on 02.10.2020.
+//
+
+import Foundation
+
+struct Feed: Codable {
+    var page: Int
+    var results: [Movie]
+    var totalResults: Int
+    var totalPages: Int
+}
+
+struct Movie: Codable {
+    var posterPath: String?
+    var adult: Bool
+    var overview: String
+    var releaseDate: String
+    var genreIds: [Int]
+    var id: Int
+    var originalTitle: String
+    var originalLanguage: String
+    var title: String
+    var backdropPath: String?
+    var popularity: Double
+    var voteCount: Int
+    var video: Bool
+    var voteAvarage: Double
+}
--- TheMovieDB/ViewController.swift
+++ TheMovieDB/ViewController.swift
@@ -12,6 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=f910e2224b142497cc05444043cc8aa4&language=en-US&page=1")
+        
+        guard url != nil else {
+            debugPrint("URL is nil!")
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error == nil && data != nil {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+                do {
+                    let feed = try decoder.decode(Feed.self, from: data!)
+                    print("This is JSON result -->> \n\(feed)")
+                } catch {
+                    debugPrint("Error JSON parsing!")
+                }
+            }
+        }
+
+        task.resume()
     }
 
 
